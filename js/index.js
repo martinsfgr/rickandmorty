e79168bef492c51308625053a84fae0268c565dd
Fagner <mauriciofagner2010@hotmail.com>
2020-05-07 04:40:33 -0300

    Renderizar os personagens no template

--- js/index.js
+++ js/index.js
@@ -3,7 +3,38 @@ const URL = "https://rickandmortyapi.com/api/character/";
 async function getCharacters() {
   const characters = await fetch(URL)
     .then(response => response.json())
-  console.log(characters.results);
+  return characters.results;
 }
 
-getCharacters();
+const cardsElement = document.querySelector("#cards");
+
+function createCharacterCard(card) {
+  const div = document.createElement("div");
+  div.style.width = "18rem";
+  div.style.margin = "1rem";
+  div.className = "card";
+  div.innerHTML = `
+  <img src="${card.image}" class="card-img-top" alt="" />
+  <div class="card-body">
+    <h5 class="card-title">${card.name}</h5>
+    <p class="card-text">
+      Situação: ${card.status}
+    </p>
+    <p class="card-text">
+      Sexo: ${card.gender}
+    </p>
+  </div>
+  `;
+  cardsElement.appendChild(div);
+}
+
+function renderCards(cards) {
+  cards.map(createCharacterCard);
+}
+
+async function main() {
+  let data = await getCharacters();
+  renderCards(data);
+}
+
+main();
