75a6bf2f396ef9af6248e1aabecaa766cdd5735f
Fagner <mauriciofagner2010@hotmail.com>
2020-05-07 14:51:03 -0300

    Adicionar filtro de personagens por status

---
+++ js/filter-status.js
@@ -0,0 +1,22 @@
+const buttonAlive = document.querySelector("#status-alive");
+const buttonDead = document.querySelector("#status-dead");
+const buttonUnknown = document.querySelector("#status-unknown");
+const buttonAll = document.querySelector("#status-all");
+
+buttonAll.addEventListener("click", () => renderCards(data));
+buttonAlive.addEventListener("click", () => renderByStatus("Alive"));
+buttonDead.addEventListener("click", () => renderByStatus("Dead"));
+buttonUnknown.addEventListener("click", () => renderByStatus("unknown"));
+
+function renderByStatus(status) {
+  const newData = filterByStatus(data, status);
+  renderCards(newData);
+}
+
+function isAlive(character, status) {
+  return character.status === status;
+}
+
+function filterByStatus(characters, status) {
+  return characters.filter(character => isAlive(character, status));
+}
